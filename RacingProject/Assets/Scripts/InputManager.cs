007fe17f2c994fc7f590de73e757b5347dfd17ad
Kirill Grushevich <kirillgrushevich@gmail.com>
2018-03-19 21:00:38 +0300

    simple car and Editor input

---
+++ RacingProject/Assets/Scripts/InputManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class InputManager : CreateSingletonGameObject<InputManager>
+{
+    public Action<object, InputType, Vector2, float> InputAction = (sender, type, vec, param) => { };
+
+    public enum InputType
+    {
+        movement,
+        tower,
+        fire
+    }
+
+
+    private Vector2 movementAxis;
+    private Vector2 towerAxis;
+
+
+
+#if UNITY_EDITOR
+
+    private void Update()
+    {
+        //Movement Axis
+        movementAxis.x = Input.GetAxis("Horizontal");
+        movementAxis.y = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(movementAxis.x) > 0.01f || Mathf.Abs(movementAxis.y) > 0.01f)
+        {
+            InputAction(this, InputType.movement, movementAxis, 0);
+        }
+
+
+        //Tower Axis
+        towerAxis.x = Input.GetAxis("Mouse X");
+        //towerAxis.y = Input.GetAxis("Mouse Y");
+        if (Mathf.Abs(towerAxis.x) > 0.01f)
+        {
+            InputAction(this, InputType.tower, towerAxis, 0);
+        }
+
+
+        //Shoot button
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            InputAction(this, InputType.fire, Vector2.zero, 0);
+        }
+
+    }
+
+#endif
+}
+
---
+++ RacingProject/Assets/Scripts/SimpleCar.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCar : MonoBehaviour
+{
+
+    public enum DriveType
+    {
+        FrontWheelDrive,
+        RearWheelDrive,
+        FourWheelDrive
+    }
+
+    [SerializeField]
+    private DriveType CarDriveType;
+
+    [SerializeField]
+    private Transform CenterOfMass;
+    [SerializeField]
+    private float maxTorque = 500f;
+    [SerializeField]
+    private float maxSteerAngle = 45f;
+    [SerializeField]
+    private Transform[] WheelMesh = new Transform[4];
+    [SerializeField]
+    private WheelCollider[] WheelCollider = new WheelCollider[4];
+
+    private Rigidbody Rig;
+
+    int fromWheelsUodate;
+    int toWheelsUpdate = 4;
+
+
+    float steer;
+    float torque;
+
+    private void Start()
+    {
+        InputManager.Instance.InputAction += UpdateAxes;
+
+        if (CarDriveType == DriveType.RearWheelDrive)
+            fromWheelsUodate = 2;
+        if (CarDriveType == DriveType.FrontWheelDrive)
+            toWheelsUpdate = 2;
+
+        Rig = GetComponent<Rigidbody>();
+        Rig.centerOfMass = CenterOfMass.localPosition;
+    }
+
+	private void OnDestroy()
+	{
+        InputManager.Instance.InputAction -= UpdateAxes;
+	}
+
+    private void UpdateAxes(object obj, InputManager.InputType type, Vector2 vec, float param)
+    {
+        if (type != InputManager.InputType.movement)
+            return;
+
+        steer = vec.x * maxSteerAngle;
+        torque = vec.y * maxTorque;
+    }
+
+
+	public void Update()
+    {
+        UpdateMeshes();
+    }
+
+    public void FixedUpdate()
+    {
+        WheelCollider[0].steerAngle = steer;
+        WheelCollider[1].steerAngle = steer;
+
+        for (int i = fromWheelsUodate; i < toWheelsUpdate; i++)
+        {
+            WheelCollider[i].motorTorque = torque;
+        }
+    }
+    public void UpdateMeshes()
+    {
+        for (int i = 0; i < WheelMesh.Length; i++)
+        {
+            Quaternion quat;
+            Vector3 pos;
+            WheelCollider[i].GetWorldPose(out pos, out quat);
+            WheelMesh[i].position = pos;
+            WheelMesh[i].rotation = quat;
+        }
+    }
+}
